4fb270d62663137422f5b727e3e8ea057c6b3d4d
Nicolas Blanc <nicolasblanc360@gmail.com>
2018-11-22 18:53:21 +0100

    Added course files

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import time
 
 import numpy as np
 import pandas as pd
+from KNN import ItemKNNCFRecommender
 import scipy.sparse as sps
 import write_submission as ws
 import validation
 
 from data_splitter import train_test_holdout
-from recommenders import TopPopRecommender, ItemKNNCFRecommender, HybridRecommender, ItemCBFKNNRecommender
+#from recommenders import TopPopRecommender, ItemKNNCFRecommender, HybridRecommender, ItemCBFKNNRecommender
+from KNN import ItemKNNCFRecommender
+import run_parameters_optimization as rpo
+from ParameterTuning import BayesianSearch
+from Base.Evaluation.Evaluator import ParallelEvaluator
 
 # Loading data
 tracks_data = pd.read_csv("data/tracks.csv")
@@ -35,20 +40,23 @@ ICM = sps.coo_matrix(tracks_data.values)
 ICM = ICM.tocsr()
 
 # Create Train et Test sets
-URM_train, URM_test = train_test_holdout(URM_all, train_perc=0.9)
-URM_train, URM_validation = train_test_holdout(URM_all, train_perc=0.9)
+URM_train, URM_test = train_test_holdout(URM_all, train_perc=0.8)
 
 # Create recommender
 
-recommender_CB = ItemCBFKNNRecommender(URM_train, ICM)
-recommender_CF = ItemKNNCFRecommender(URM_train)
-hybrid_recommender = HybridRecommender(URM_all, URM_train, recommender_CB, recommender_CF)
-hybrid_recommender.fit()
+# recommender_CB = ItemCBFKNNRecommender(URM_train, ICM)
+# recommender_CF = ItemKNNCFRecommender(URM_train)
+# hybrid_recommender = HybridRecommender(URM_all, URM_train, recommender_CB, recommender_CF)
+# hybrid_recommender.fit()
+Evaluator = ParallelEvaluator(URM_test,[10])
+Recommender = ItemKNNCFRecommender.ItemKNNCFRecommender(URM_train=URM_train)
+Bayesian = BayesianSearch.BayesianSearch(Recommender, Evaluator)
+rpo.runParameterSearch_Collaborative(recommender_class=Recommender, URM_train=URM_train, metric_to_optimize="MAP", evaluator_validation=Evaluator)
 
 # # ef.evaluate_algorithm(URM_test, topPopRecommender)
 #
 # # Evaluation of algorithm
-# validator = validation.Validation(URM_train, URM_validation, URM_test, recommender)
+# validator = validation.Validation(URM_train, URM_test, recommender)
 # optim_shrink, optim_k = validator.parameters_tunning()
 #
 # print("Optimum k : {}".format(optim_k))
@@ -56,8 +64,8 @@ hybrid_recommender.fit()
 #
 # recommender.fit(shrink=optim_shrink, k=optim_k)
 
-target_data = pd.read_csv('data/target_playlists.csv')
+#target_data = pd.read_csv('data/target_playlists.csv')
 
-ws.write_submission(target_data, hybrid_recommender, 'output/submission.csv', at=10)
+#ws.write_submission(target_data, hybrid_recommender, 'output/submission.csv', at=10)
 
 #print(hybrid_recommender.evaluateRecommendations(URM_test))
--- validation.py
+++ validation.py
@@ -1,16 +1,20 @@
 import time
 import evaluation_function as ef
 from matplotlib import pyplot as plt
+import multiprocessing as mult
+import itertools
+
 
 
 class Validation:
-    def __init__(self, train_data, validation_data, test_data, test_recommender, test_recommender_2=None):
+    def __init__(self, train_data, test_data, test_recommender, test_recommender_2=None):
         self.train = train_data
-        self.validation = validation_data
         self.test = test_data
         self.recommender = test_recommender
         self.optim_k = 10
         self.optim_shrink = 1
+        self.MAP_parameters = []
+
         if test_recommender_2:
             self.recommender_2 = test_recommender_2
 
@@ -28,49 +32,73 @@ class Validation:
         print("Wrong implementation speed is {:.2f} usr/sec".format(n_users_to_test / (end_time - start_time)))
 
     def parameters_tunning(self, shrink=True, neigh_nb=True):
+        k_nb = [5, 10, 20, 30, 50, 100, 200, 500]
+        shrink_term_nb = [0.1, 0.5, 1, 2, 4, 6, 8, 10]
 
-        if neigh_nb:
+        combinations = list(itertools.product(k_nb, shrink_term_nb))
 
-            print("------------------")
-            print("Tunning of k neighbors number")
-            print("------------------")
+        print("------------------")
+        print("Tunning parameters")
+        print("------------------")
 
-            k_nb = [5, 10, 20, 30, 50, 100, 200, 500]
-            MAP_per_k = []
-            for k in k_nb:
-                self.recommender.fit(shrink=self.optim_shrink, k=k)
-                result_dict = ef.evaluate_algorithm(self.validation, self.recommender)
-                MAP_per_k.append(result_dict["MAP"])
+        MAP_parameters = []
+        for combination in combinations:
+            self.recommender.fit(shrink=combination[1], k=combination[0])
+            result_dict = ef.evaluate_algorithm(self.test, self.recommender)
+            MAP_parameters.append(result_dict["MAP"])
 
-            # Plot the MAP result for each k number
-            plt.plot(MAP_per_k)
-            plt.xlabel("k number")
-            plt.ylabel("MAP")
-            plt.show()
+        # Plot the MAP result for each k number
+        plt.plot(MAP_parameters)
+        plt.xlabel("Parameters combinations")
+        plt.ylabel("MAP")
+        plt.show()
 
-            optim_k_index = MAP_per_k.index(max(MAP_per_k))
-            self.optim_k = k_nb[optim_k_index]
+        optim_k_index = MAP_parameters.index(max(MAP_per_k))
+        self.optim_k = combinations[optim_k_index][0]
+        self.optim_shrink = combinations[optim_k_index][1]
 
-        if shrink:
-            print("------------------")
-            print("Tunning of shrink")
-            print("------------------")
+        return self.optim_shrink, self.optim_k
 
-            shrink_term_nb = [0.1, 0.5, 1, 2, 4, 6, 8, 10]
-            MAP_per_shrink = []
-            for shrink in shrink_term_nb:
-                self.recommender.fit(shrink=shrink, k=self.optim_k)
-                result_dict = ef.evaluate_algorithm(self.validation, self.recommender)
-                MAP_per_shrink.append(result_dict["MAP"])
+    def recommend_evaluate_k(self, tuple):
+        if tuple:
+            self.recommender.fit(shrink=tuple[1], k=tuple[0])
+            result_dict = ef.evaluate_algorithm(self.test, self.recommender)
+            self.MAP_parameters.append(result_dict["MAP"])
+        else:
+            print("No tuple given")
+            return
 
-            # Plot the MAP result for each k number
-            plt.plot(MAP_per_shrink)
-            plt.xlabel("Shrink value")
-            plt.ylabel("MAP")
-            plt.show()
+    def parameters_tunning_parallellized( self ):
+        """ parameters_tunning_paralellized use parallelized computing to tune parameters as the number
+        of neighbors k or the value of the shrink parameter.
+        :param shrink: If true, will be evaluated
+        :param neigh_nb: If true, will be evaluated
+        :return: Optim_k and Optim_shrink and it will update de value of optim_k or optim_shrink
+        """
 
-            optim_shrink_index = MAP_per_shrink.index(max(MAP_per_shrink))
-            self.optim_shrink = shrink_term_nb[optim_shrink_index]
+        # TODO : Verbose mode
+        number_of_workers = 4
+        k_nb = [5, 10, 20, 30, 50, 100, 200, 500]
+        shrink_term_nb = [0.1, 0.5, 1, 2, 4, 6, 8, 10]
+        combinations = list(itertools.product(k_nb, shrink_term_nb))
+        self.MAP_parameters = []
+
+
+        print("------------------")
+        print("Tunning parameters")
+        print("------------------")
+
+        with mult.Pool(number_of_workers) as p:
+            p.starmap(self.recommend_evaluate_k, k_nb)
+
+        # Plot the MAP result for each k number
+        plt.plot(self.MAP_parameters)
+        plt.xlabel("Parameters combinations")
+        plt.ylabel("MAP")
+        plt.show()
+
+        optim_k_index = MAP_per_k.index(max(MAP_per_k))
+        self.optim_k = k_nb[optim_k_index]
 
         return self.optim_shrink, self.optim_k
 
